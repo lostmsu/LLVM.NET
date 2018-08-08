5ac08ccb2a2cc489ecafbfd786fa297d1e7410d9
Victor Milovanov <lostfreeman@gmail.com>
2018-08-08 15:46:04 -0700

    bumped version of NativeLibrary to 3.3

--- LLVM.NativeLibrary/Properties/AssemblyInfo.cs
+++ LLVM.NativeLibrary/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LLVM .NET")]
-[assembly: AssemblyCopyright("Copyright © Victor Milovanov 2013")]
+[assembly: AssemblyCopyright("Copyright © Victor Milovanov 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("3.3.*")]
 [assembly: AssemblyDescription("This assembly provides native LLVM dynamic libraries for Win32 and x64")]
