15ec2feafaca048f92ab53a4049f3f45d7bf58ad
Victor Milovanov <lostfreeman@gmail.com>
2018-08-08 15:37:46 -0700

    detect missing Visual C++ runtime library

--- LLVM.NativeLibrary/LLVMDLL.cs
+++ LLVM.NativeLibrary/LLVMDLL.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using Lost.Native;
+﻿namespace LLVM.NativeLibrary {
+    using System;
+    using System.ComponentModel;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
 
-namespace LLVM.NativeLibrary
-{
+    using Lost.Native;
+
     public static class LLVMDLL
     {
+        const int ERROR_MOD_NOT_FOUND = 0x0000007E;
         public static void Load()
         {
             var assembly = Assembly.GetExecutingAssembly();
-            DynamicLibrary.Load(assembly, "LLVM-3.3", "LLVM.NativeLibrary");
+            try {
+                DynamicLibrary.Load(assembly, "LLVM-3.3", "LLVM.NativeLibrary");
+            } catch (Win32Exception e) when (e.NativeErrorCode == ERROR_MOD_NOT_FOUND && !IsVisualCRuntimeAvailable()) {
+                throw new NotSupportedException("The latest Visual C++ 2015 redistributable must be installed", innerException: e);
+            }
         }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static bool IsVisualCRuntimeAvailable() =>
+            LoadLibrary("MSVCP140") == IntPtr.Zero
+            || LoadLibrary("VCRUNTIME140.dll") == IntPtr.Zero;
+
+        [DllImport("kernel32")]
+        static extern IntPtr LoadLibrary(string path);
     }
 }
